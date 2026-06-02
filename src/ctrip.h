cf38ed35645d01c8dff6791a2831972828363e7c
Warrick <1016weicheng@gmail.com>
2026-06-02 19:26:05 +0800

    release ror-1.4.11 xredis-3.2.4

--- src/ctrip.h
+++ src/ctrip.h
@@ -8,7 +8,7 @@
  #ifndef SRC_CTRIP_H_
  #define SRC_CTRIP_H_
  
- #define XREDIS_VERSION "3.2.2"
+ #define XREDIS_VERSION "3.2.4"
  #define CONFIG_DEFAULT_SLAVE_REPLICATE_ALL 0
  
  void xslaveofCommand(client *c);
