6de0b7f2d110d8bcb2c0cf901b3f4db1aab0ba1a
Warrick <1016weicheng@gmail.com>
2026-07-14 14:50:42 +0800

    [test] remove redundancy test api

--- tests/modules/propagate.c
+++ tests/modules/propagate.c
@@ -149,24 +149,7 @@ int propagateTestSimpleCommand(RedisModuleCtx *ctx, RedisModuleString **argv, in
     return REDISMODULE_OK;
 }
 
-int propagateTestSingleCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
-{
-    REDISMODULE_NOT_USED(argv);
-    REDISMODULE_NOT_USED(argc);
 
-    RedisModule_Replicate(ctx,"INCR","c","single-counter");
-    RedisModule_ReplyWithSimpleString(ctx,"OK");
-    return REDISMODULE_OK;
-}
-
-int propagateTestNoReplicateCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
-{
-    REDISMODULE_NOT_USED(argv);
-    REDISMODULE_NOT_USED(argc);
-
-    RedisModule_ReplyWithSimpleString(ctx,"OK");
-    return REDISMODULE_OK;
-}
 
 int propagateTestMixedCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
 {
@@ -256,15 +239,7 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
                 "",1,1,1) == REDISMODULE_ERR)
             return REDISMODULE_ERR;
 
-    if (RedisModule_CreateCommand(ctx,"propagate-test.single",
-                propagateTestSingleCommand,
-                "",1,1,1) == REDISMODULE_ERR)
-            return REDISMODULE_ERR;
 
-    if (RedisModule_CreateCommand(ctx,"propagate-test.noreplicate",
-                propagateTestNoReplicateCommand,
-                "",1,1,1) == REDISMODULE_ERR)
-            return REDISMODULE_ERR;
 
     if (RedisModule_CreateCommand(ctx,"propagate-test.mixed",
                 propagateTestMixedCommand,
